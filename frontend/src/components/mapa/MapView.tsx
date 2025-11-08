00c66f79427f5f40e663f22fd77d9e5a09da5891
SebasZ03 <pzunigav@unsa.edu.pe>
2025-11-08 09:28:59 -0500

    modificamos front para conectar con back

--- frontend/src/components/mapa/MapView.tsx
+++ frontend/src/components/mapa/MapView.tsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import mapData from "./mapData.json";
-
-import iconUrl from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 const DefaultIcon = L.icon({
-  iconUrl,
-  shadowUrl: iconShadow,
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
   iconAnchor: [12, 41],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+interface Punto {
+  id: number;
+  nombre: string;
+  latitud: number;
+  longitud: number;
+}
+
 const MapView: React.FC = () => {
+  const [puntos, setPuntos] = useState<Punto[]>([]);
   const centerPosition: [number, number] = [-16.39889, -71.535];
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/puntos")
+      .then((res) => res.json())
+      .then((data) => setPuntos(data));
+  }, []);
 
   return (
     <MapContainer center={centerPosition} zoom={13} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {mapData.map((point) => (
-        <Marker key={point.id} position={[point.lat, point.lng]}>
-          <Popup>{point.name}</Popup>
+      {puntos.map((p) => (
+        <Marker key={p.id} position={[p.latitud, p.longitud]}>
+          <Popup>{p.nombre}</Popup>
         </Marker>
       ))}
     </MapContainer>
